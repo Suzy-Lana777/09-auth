348e6190431f9896f977cf2b774d0e9b49343934
Suzy-Lana777 <reutasvetlana382@gmail.com>
2025-08-31 16:54:57 +0300

    fix api

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,9 +1,16 @@
 // lib/api.ts
 
+// import axios from "axios";
+
+// export const nextServer = axios.create({
+//   baseURL: "https://notehub-api.goit.study", 
+//   headers: { "Content-Type": "application/json" },
+//   withCredentials: true,
+// });
+
 import axios from "axios";
 
 export const nextServer = axios.create({
-  baseURL: "https://notehub-api.goit.study", 
-  headers: { "Content-Type": "application/json" },
+  baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
   withCredentials: true,
 });
